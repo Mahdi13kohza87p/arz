7bb43a856e22e414a83ac1945dfe22f92579ec47
Mahdi13kohza87p <109549142+Mahdi13kohza87p@users.noreply.github.com>
2024-07-29 05:30:41 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 const apiUrl = 'https://api.api-code.ir/api/arzapp/index.php?page=1';
-const coinTable = document.getElementById('coin-table');
-const coinData = document.getElementById('coin-data');
 
 fetch(apiUrl)
-   .then(response => response.json())
-   .then(data => {
-        data.forEach(item => {
-            const row = document.createElement('tr');
-            row.innerHTML = `
-                <td>${item.name}</td>
-                <td>${item.price}</td>
-                <td>${item.volume}</td>
-            `;
-            coinData.appendChild(row);
-        });
-    })
-   .catch(error => console.error('Error:', error));
+  .then(response => response.json())
+  .then(data => {
+    const result = data.result;
+    result.forEach(item => {
+      console.log(item.price.nameEn, item.price.priceUSDT);
+    });
+  })
+  .catch(error => console.error('Error:', error));
